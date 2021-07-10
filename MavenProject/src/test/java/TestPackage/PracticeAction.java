299497656523c5547c145cda9e4da5c1afe3a723
Biplov <biplov143@gmail.com>
2021-07-10 21:25:11 +0530

    Browser quit code added

--- MavenProject/src/test/java/TestPackage/PracticeAction.java
+++ MavenProject/src/test/java/TestPackage/PracticeAction.java
@@ -7,21 +7,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 
 public class PracticeAction {
+
+  WebDriver driver;	
 	
   @Test
   public void f() {
 	   
   }
+  
   @BeforeMethod
   public void launch() {
 	  
 	  System.setProperty("webdriver.gecko.driver","F:\\Software\\Drivers\\geckodriver.exe");
-	   WebDriver driver = new FirefoxDriver();
-	   driver.manage().window().maximize();
+	  driver = new FirefoxDriver();
+	  driver.manage().window().maximize();
   }
 
   @AfterMethod
   public void afterMethod() {
+	  driver.quit();		
   }
 
 }
